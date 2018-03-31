7f888b0e0fd24a6a6a9086ca7b392d947880d0d3
Arek <arek123113@gmail.com>
2018-03-31 14:01:47 +0200

    Dodaj pliki projektów.

---
+++ ImageGallery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImageGallery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
